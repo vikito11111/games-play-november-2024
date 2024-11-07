50f4db8646a2e9b8879625b45337097b929f0fd5
vikito11111 <vikito11111@gmail.com>
2024-11-07 14:06:28 +0200

    Add no games found message

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -14,12 +14,7 @@ export default function GameCatalog() {
         /* <!-- Catalogue --> */
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
-
-            {games.map(game => <GameCatalogItem key={game._id} {...game} />)}
-
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0 ? games.map(game => <GameCatalogItem key={game._id} {...game} />) : <h3 className="no-articles">No games yet</h3>}
         </section>
     );
 }

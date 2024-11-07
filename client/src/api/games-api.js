7fbc57e680b688c7696fbb93765ad9cae45e4bd0
vikito11111 <vikito11111@gmail.com>
2024-11-07 14:02:33 +0200

    List all games

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -2,6 +2,10 @@ import * as request from './requester';
 
 const GAMES_BASE_URL = 'http://localhost:3030/jsonstore/games';
 
-export function getAll() {
-    return request.get(GAMES_BASE_URL);
+export async function getAll() {
+    const result = await request.get(GAMES_BASE_URL);
+
+    const games = Object.values(result);
+
+    return games;
 };
